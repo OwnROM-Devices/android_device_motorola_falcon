a8fc3eeeeeba2d663b659a9de22a91e2c198551a
Victor Linfield <vilinfield@gmail.com>
2015-05-22 16:35:02 -0230

    Update ownrom.mk

--- ownrom.mk
+++ ownrom.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common ownrom stuff.
 $(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
