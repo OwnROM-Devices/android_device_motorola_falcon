6c54ad08114e20897be13188f30f26b64e716c51
Akhil Narang <akhilnarang.1999@gmail.com>
2016-04-17 14:49:42 +0530

    falcon: Ownify
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- ownrom.mk
+++ ownrom.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OwnROM stuff.
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+
+PRODUCT_NAME := ownrom_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
